137d943cf7679976a763b745dc8fe8c93b1a700f
Robert Graman <rjgxfv@mail.missouri.edu>
2019-02-03 01:06:25 -0600

    added closure playground file

--- closure.playground/Contents.swift
+++ closure.playground/Contents.swift
@@ -1,1 +1,14 @@
+import UIKit
 
+struct MathOperation{
+    var units: String
+    var operation: (Double, Double)-> Double
+    
+    init?(units:String, operation: @escaping (Double, Double)->Double){
+        if(units==""){
+            return nil
+        }
+        self.units = units
+        self.operation = operation
+    }
+}
